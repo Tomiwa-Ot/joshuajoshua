d50691a07dcb4cc2b8c1ca40f340ea66bfc3f562
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-03-02 19:52:08 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:JoshuaJoshua/main.dart';
+import 'package:joshua_joshua/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
