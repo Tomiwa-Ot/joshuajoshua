3bc061c6cd1ce09cdf646352b4255387fafe7b31
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-03-03 01:04:14 +0100

    login persistence

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:overlay_support/overlay_support.dart';
+import 'package:joshua_joshua/screens/homepage.dart';
+import 'package:joshua_joshua/screens/auth.dart';
 
 void main() {
   runApp(Init());
@@ -14,14 +17,26 @@ class Init extends StatefulWidget {
 
 class _InitState extends State<Init> {
 
+  bool _isLoggedIn = false;
+
   Future<FirebaseApp> initializeFirebase() async{
     FirebaseApp firebaseApp = await Firebase.initializeApp();
     return firebaseApp;
   }
 
+  Future checkAuthStatus() async {
+    SharedPreferences authStatus = await SharedPreferences.getInstance();
+    if (authStatus.getBool("isLoggedIn") != null) {
+      setState(() {
+        _isLoggedIn = authStatus.getBool("isLoggedIn");
+      });
+    }
+  }
+
   void initState() {
     super.initState();
     initializeFirebase();
+    checkAuthStatus();
   }
 
   @override
@@ -29,10 +44,12 @@ class _InitState extends State<Init> {
     return OverlaySupport(
       child: MaterialApp(
         theme: ThemeData(
-          primarySwatch: Colors.black,
+          primarySwatch: Colors.grey,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-      )
+        home: _isLoggedIn ? Homepage() : Auth(),
+        color: Colors.black
+      ),
     );
   }
 }
