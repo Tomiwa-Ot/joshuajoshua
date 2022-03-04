14e2ca8be4df7661b6707484ac1b64205816550e
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-03-04 11:39:19 +0100

    Create receipt.dart

---
+++ lib/screens/receipt.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Receipt extends StatefulWidget {
+
+  @override
+  _ReceiptState createState() => _ReceiptState();
+}
+
+class _ReceiptState extends State<Receipt> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
