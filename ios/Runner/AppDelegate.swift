6e878343fb030d8488fa4d32f92fa0d041187240
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-03-04 11:39:56 +0100

    api key updated

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-    GMSServices.provideAPIKey("AIzaSyAskh4n6UiJ8WEOGq8G68fAqo_OJ4KiO1c")
+    GMSServices.provideAPIKey("AIzaSyBNnzq0CRkSPs10L-I3o1Zxjux8_OAyFhM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
