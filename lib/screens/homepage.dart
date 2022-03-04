64d1d9cdac74616a3cebfcc783e4c1f899b83cc4
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-03-04 11:41:34 +0100

    geocoding

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:joshua_joshua/screens/wallet.dart';
 import 'package:joshua_joshua/util/util.dart';
+import 'package:joshua_joshua/screens/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'dart:async';
 
 class Homepage extends StatefulWidget {
@@ -17,6 +21,8 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
 
+  String location, email;
+  bool _isLocationSet = false;
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   User user = FirebaseAuth.instance.currentUser;
   Stream<DocumentSnapshot> stream;
@@ -25,7 +31,7 @@ class _HomepageState extends State<Homepage> {
   CameraPosition _cameraPosition;
   CameraPosition _initialCameraPosition = CameraPosition(
     zoom: CAMERA_ZOOM,
-    target: defaultLatLng
+    target: defaultLatLng,
   );
 
   void initState() {
@@ -40,96 +46,30 @@ class _HomepageState extends State<Homepage> {
   }
 
   Future gotoCurrentLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-
-    if (!serviceEnabled) {
-      showSnackBar("Location permissions are denied", context);
+    if (!await Permission.location.isGranted) {
+      await Permission.location.request();
       return;
     }
 
-    if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever) {
-      showSnackBar("Location permissions are denied, we cannot request permissions", context);
+    if (!await Geolocator.isLocationServiceEnabled()) {
+      showSnackBar("Location services are not enabled", context);
       return;
     }
-    
+
     Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
     LatLng latLng = LatLng(position.latitude, position.longitude);
     _cameraPosition = CameraPosition(target: latLng, zoom: CAMERA_ZOOM);
+    List<Placemark> placemarks = await placemarkFromCoordinates(latLng.latitude, latLng.longitude);
+    setState(() {
+      location = "${placemarks.first.street.toString()} ${placemarks.first.subAdministrativeArea.toString()} ${placemarks.first.postalCode.toString()} ${placemarks.first.administrativeArea.toString()}";
+      _destination = Marker(
+        markerId: const MarkerId('destination'),
+        infoWindow: InfoWindow(title: 'My Location'),
+        position: latLng
+      );
+      _isLocationSet = true;
+    });
     _controller.animateCamera(CameraUpdate.newCameraPosition(_cameraPosition));
-    _destination = Marker(
-      markerId: MarkerId('destination'),
-      infoWindow: InfoWindow(title: 'My Location'),
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-      position: latLng
-    );
-    
-    showModalBottomSheet(
-      context: context,
-      backgroundColor: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(15.0))
-      ),
-      builder: (context) {
-        return StatefulBuilder(
-          builder: (context, setState) {
-           return Column(
-             children: [
-               Padding(
-                 padding: EdgeInsets.all(8.0),
-                 child: Row(
-                   children: [
-                     Expanded(
-                       child: Container(),
-                     ),
-                     IconButton(
-                       icon: Icon(CupertinoIcons.clear, color: Colors.black,),
-                       onPressed: () {
-                         Navigator.of(context).pop();
-                       },
-                     )
-                   ],
-                 ),
-               ),
-               SizedBox(
-                 width: 20.0,
-               ),
-               Container(
-                 decoration: BoxDecoration(
-                   color: Colors.black,
-                   borderRadius: BorderRadius.circular(30.0)
-                 ),
-                 alignment: Alignment.center,
-                 width: 400.0,
-                 height: 55.0,
-                 child: ElevatedButton(
-                   child: Row(
-                     mainAxisSize: MainAxisSize.min,
-                     children: [
-                       Icon(CupertinoIcons.search, color: Colors.white,),
-                       SizedBox(
-                         width: 5.0,
-                       ),
-                       Text("Proceed",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w800
-                        ),
-                       )
-                     ],
-                   ),
-                   onPressed: () {
-                     
-                   },
-                 ),
-               )
-             ],
-           );
-          },
-        );
-      }
-    );
   }
 
   Future searchForNearbyBarbers() async {
@@ -178,6 +118,9 @@ class _HomepageState extends State<Homepage> {
                     );
                   }
 
+                  setState(() {
+                    email = snapshot.data['fullname'];
+                  });
                   return ListTile(
                     leading: CircleAvatar(
                       radius: 25.0,
@@ -196,7 +139,8 @@ class _HomepageState extends State<Homepage> {
                     ),
                     subtitle: Text(snapshot.data['email'],
                       style: TextStyle(
-                        color: Colors.white
+                        color: Colors.white,
+                        fontSize: 12.0
                       ),
                     ),
                   );
@@ -205,8 +149,21 @@ class _HomepageState extends State<Homepage> {
             ),
             Expanded(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  ListTile(
+                    leading: Icon(CupertinoIcons.money_dollar, color: Colors.black),
+                    title: Text("Wallet",
+                      style: TextStyle(
+                        fontSize: 17.0
+                      ),
+                    ),
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                        Wallet(email: email,)
+                      )); 
+                    },
+                  ),
                   ListTile(
                     leading: Icon(Icons.settings_outlined, color: Colors.black),
                     title: Text("Settings",
@@ -235,8 +192,8 @@ class _HomepageState extends State<Homepage> {
                       SharedPreferences authStatus = await SharedPreferences.getInstance();
                       authStatus.setBool("isLoggedIn", false);
                       await FirebaseAuth.instance.signOut();
-                      // Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
-                      //   Auth()), (route) => false);
+                      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
+                        Auth()), (route) => false);
                     },
                   )
                 ],
@@ -254,6 +211,22 @@ class _HomepageState extends State<Homepage> {
               if (_destination != null) _destination,
               if (_source != null) _source
             },
+            zoomControlsEnabled: false,
+            zoomGesturesEnabled: true,
+            onTap: (LatLng _latLng) async {
+              _cameraPosition = CameraPosition(target: _latLng, zoom: CAMERA_ZOOM);
+              List<Placemark> placemarks = await placemarkFromCoordinates(_latLng.latitude, _latLng.longitude);
+              setState(() {
+                location = "${placemarks.first.street.toString()} ${placemarks.first.subAdministrativeArea.toString()} ${placemarks.first.postalCode.toString()} ${placemarks.first.administrativeArea.toString()}";
+                _destination = Marker(
+                  markerId: const MarkerId('destination'),
+                  infoWindow: InfoWindow(title: 'My Location'),
+                  position: _latLng
+                );
+                _isLocationSet = true;
+              });
+              _controller.animateCamera(CameraUpdate.newCameraPosition(_cameraPosition));
+            },
             onMapCreated: (GoogleMapController controller) {
               _controller = controller;
             },
@@ -265,49 +238,115 @@ class _HomepageState extends State<Homepage> {
               child: Container(
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.circular(22.0),
+                  borderRadius: BorderRadius.circular(27.0),
                 ),
                 child: CircleAvatar(
                   backgroundColor: Colors.white,
-                  child: Icon(CupertinoIcons.line_horizontal_3, color: Colors.black,),
-                  radius: 20.0,
+                  child: Icon(CupertinoIcons.line_horizontal_3, color: Colors.black, size: 25.0,),
+                  radius: 25.0,
                 ),
               ),
-              onTap: () => _scaffoldKey.currentState.openDrawer(),
+              onTap: () => _scaffoldKey.currentState.openDrawer()
             ),
           ),
-          // cancel search & current location button
-          Positioned(
-            top: 45.0,
-            left: 37.0,
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(3.0),
-                color: Colors.white,
-              ),
-              child: TextField(
-                cursorColor: Colors.black,
-                // onChanged: ,
-                decoration: InputDecoration(
-                  icon: Container(
-                    margin: EdgeInsets.only(left: 20.0, top: 5.0),
-                    width: 10.0,
-                    height: 10.0,
-                    child: Icon(CupertinoIcons.search, color: Colors.black,),
-                  ),
-                  hintText: "Search",
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.only(left: 15.0, top: 16.0)
+          DraggableScrollableSheet(
+            initialChildSize: 0.28,
+            minChildSize: 0.28,
+            builder: (context, scrollController) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(235, 235, 235, 1),
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20.0), topRight: Radius.circular(20.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.8),
+                      offset: Offset(3, 2),
+                      blurRadius: 7
+                    )
+                  ]
                 ),
-              ),
-            ),
+                child: _isLocationSet ? ListView(
+                  children: [
+                    ListTile(
+                      title: Container(
+                        margin: EdgeInsets.only(left: 30.0),
+                        child: Text(location,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w800,
+                            fontSize: 18.0
+                          ),
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(CupertinoIcons.clear),
+                        color: Colors.black,
+                        onPressed: () {
+                          setState(() {
+                            _destination = null;
+                            _isLocationSet = false;
+                          });
+                        },
+                      ),
+                    ),
+                    ListTile(
+                      title: Container(
+                        height: 50.0,
+                        margin: EdgeInsets.only(left: 30.0, right: 30.0),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.black
+                        ),
+                        child: Text("Proceed",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900,
+                            fontSize: 22.0
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+
+                      },
+                    )
+                  ],
+                ) : ListView(
+                  children: [
+                    Container(
+                      height: 60.0,
+                      color: Colors.grey.withOpacity(0.3),
+                      margin: EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
+                      child: TextField(
+                      cursorColor: Colors.black,
+                      onChanged: (val) {
+                        
+                      },
+                      textInputAction: TextInputAction.go,
+                      decoration: InputDecoration(
+                        icon: Container(
+                          margin: EdgeInsets.only(left: 20.0, bottom: 15.0),
+                          width: 10.0,
+                          height: 10.0,
+                          child: Icon(CupertinoIcons.search, color: Colors.black,),
+                        ),
+                        hintText: "Search",
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.only(left: 15.0, top: 14.0)
+                      ),
+                    ),
+                  )
+                  ],
+                )
+              );
+            },
           )
         ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.black,
-        onPressed: () => gotoCurrentLocation,
+        onPressed: () {
+          gotoCurrentLocation();
+        },
         child: Icon(CupertinoIcons.location_fill, color: Colors.white,),
       ),
     );
