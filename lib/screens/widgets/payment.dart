367b4a30943b56857c9c4b870f328af3f5fb838e
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-03-08 21:19:54 +0100

    Create payment.dart

---
+++ lib/screens/widgets/payment.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Payment extends StatefulWidget {
+
+  @override
+  _PaymentState createState() => _PaymentState();
+}
+
+class _PaymentState extends State<Payment> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
